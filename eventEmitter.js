f2eb72bcd514745f9d8053430c40d272579fd05d
Dhatchanamoorthy S <156570369+dhatchan96@users.noreply.github.com>
2025-10-06 23:57:52 +0530

    Add files via upload

---
+++ eventEmitter.js
@@ -0,0 +1,30 @@
+// Simple event emitter for global communication
+class EventEmitter {
+  constructor() {
+    this.events = {};
+  }
+
+  on(event, callback) {
+    if (!this.events[event]) {
+      this.events[event] = [];
+    }
+    this.events[event].push(callback);
+  }
+
+  off(event, callback) {
+    if (this.events[event]) {
+      this.events[event] = this.events[event].filter(cb => cb !== callback);
+    }
+  }
+
+  emit(event, data) {
+    if (this.events[event]) {
+      this.events[event].forEach(callback => callback(data));
+    }
+  }
+}
+
+// Create a global instance
+const globalEventEmitter = new EventEmitter();
+
+export default globalEventEmitter;
